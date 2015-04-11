8278f6f7f7f350c3e6985f427dd98a8b920c7754
unknown <x14125439@scr3-045.student.nci.col>
2015-04-11 16:22:07 +0100

    work during saturday class 11/04

--- css/autoogumienie.js
+++ css/autoogumienie.js
@@ -1,3 +1,22 @@
+
 jQuery(document).ready(function($){
-   $('#cont').pwstabs();        
+   $('#cont').pwstabs({
+		effect: 'slideleft',
+		conteinerHeight: '600px',
+		horizontalPosition: 'top',
+		verticalPosition: 'left'
+   });
+	$('.marquee').marquee({
+		//speed in milliseconds of the marquee
+		duration: 15000,
+		//gap in pixels between the tickers
+		gap: 50,
+		//time in milliseconds before the marquee will start animating
+		delayBeforeStart: 0,
+		//'left' or 'right'
+		direction: 'left',
+		//true or false - should the marquee be duplicated to show an effect of continues flow
+		duplicated: true
+	});
 });
+
